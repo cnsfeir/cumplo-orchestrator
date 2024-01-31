4ba4c88d4767bbe33e315ececdb019d27bd1de8c
Cristobal Sfeir <cnsfeir@uc.cl>
2024-01-30 21:12:25 -0300

    🔖 Release version 0.1.0

--- cumplo_orchestrator/__init__.py
+++ cumplo_orchestrator/__init__.py
@@ -3,4 +3,4 @@ A centralized coordination service designed to seamlessly integrate,
 manage, scale and automate workflows across backend services
 """
 
-__version__ = "0.0.1"
+__version__ = "0.1.0"
